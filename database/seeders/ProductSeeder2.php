7a62503392e43f8d83e7be56b3b0894e4ccd435a
userferdi <ferdi.maulana@gmail.com>
2021-04-12 22:03:46 +0700

    maap atuh bro

--- database/seeders/ProductSeeder2.php
+++ database/seeders/ProductSeeder2.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\FlashSale;
 
-class ProductSeeder1 extends Seeder
+class ProductSeeder2 extends Seeder
 {
     /**
      * Run the database seeds.
